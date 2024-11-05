5d3e10966d214d68ac8f60b737681cc721776d07
cladren123 <gold3987@naver.com>
2024-11-05 15:51:19 +0900

    test

---
+++ oms2/src/main/java/com/yogosaza/oms2/Oms2Application.java
@@ -0,0 +1,13 @@
+package com.yogosaza.oms2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oms2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oms2Application.class, args);
+	}
+
+}
---
+++ oms2/src/test/java/com/yogosaza/oms2/Oms2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yogosaza.oms2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oms2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
